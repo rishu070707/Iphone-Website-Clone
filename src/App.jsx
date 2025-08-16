aa75a4429fffd1a7a0d1b6d26e08b339f5b6b6dc
rishu070707 <rishabhsingh301220@gmail.com>
2025-08-16 19:46:02 +0530

    highlights-fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import Highlights from "./components/highlights";
+import Highlights from "./components/Highlights";
 import Model from "./components/Model"
  const App = () => { 
   return(
